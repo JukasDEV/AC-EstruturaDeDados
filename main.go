7b6915b73b8e6bd9e0e646b563f545f411f2d566
Joao Lucas Curvello <103194851+JukasDEV@users.noreply.github.com>
2023-08-09 09:07:00 -0300

    todas funcoes AC1

--- main.go
+++ main.go
@@ -1,43 +1,51 @@
 package main
 
-import {
+import (
 	"fmt"
-}
+)
 
-// Elabore uma função e_primo() que retorna se um número é primo ou não. Caso o número não seja primo, liste todos os números pelos quais ele é divisível.
-
-
-func main(){
-
-// fmt.println(e_primo)
-e_primo()
-
+func main() {
+	fmt.Println("Hello, World!")
+	// e_primo()
+	e_bissexto()
 }
 
 func e_primo() {
 
-var i, numero int
+	var i, numero int
 
-var primo bool = true
+	var primo bool = true
 
-fmt.Print("Informe um número inteiro : ")
-  fmt.Scanln(&numero)
+	fmt.Print("Informe um número inteiro : ")
+	fmt.Scanln(&numero)
 
-  for i = 2; i <= int(numero / 2); i++ {
-	if numero % i == 0 {
-	  primo = false
-	  break
+	for i = 2; i <= int(numero/2); i++ {
+		if numero%i == 0 {
+			primo = false
+			break
+		}
 	}
-  }    
-   
-  if(primo){
-	fmt.Print("O número informado é primo")
-  }else{
-	fmt.Print("O número informado não é primo")
-  }
+
+	if primo {
+		fmt.Print("O número informado é primo")
+	} else {
+		fmt.Print("O número informado não é primo")
+	}
 }
 
+func e_bissexto() {
+	var Ano int
 
+	fmt.Print("Informe um Ano : ")
+	fmt.Scanln(&Ano)
 
+	if Ano%4 == 0 {
+		if Ano%100 == 0 && Ano%400 != 0 {
+			fmt.Print("Não é Bissexto")
+		}
+		fmt.Print("é bissexto")
+	} else {
+		fmt.Print("Não é Bissexto")
 
+	}
 }

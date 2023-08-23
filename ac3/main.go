1f91fe4e728d9024d3e4b2e76cb2d932cf4d968c
joaocurvelolinera <103194851+JukasDEV@users.noreply.github.com>
2023-08-23 17:17:44 -0300

    ac3

---
+++ ac3/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"ac3/contato"
+	"ac3/operacoes"
+)
+
+
+
+func main() {
+	var contatos [5]contato.Contato
+	var nome, email, opcao, novoEmail string
+	var indice int
+
+	leitor := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Lista de contatos!")
+	for {
+		fmt.Print("Digite (1) para adicionar, (2) para remover, (3) para alterar um email, (4) exibir todos os contatos ou qualquer outra coisa para sair: ")
+		fmt.Scanln(&opcao)
+
+		switch opcao {
+		case "1":
+			fmt.Print("Informe o seu nome: ")
+			nome, _ = leitor.ReadString('\n')
+
+			fmt.Print("Informe o seu email: ")
+			fmt.Scanln(&email)
+
+			fmt.Println(nome, email)
+			contatos = operacoes.AdicionaContato(contato.Contato{Nome: nome, Email: email}, contatos)
+		case "2":
+			contatos = operacoes.ExcluiContato(contatos)
+
+		case "3":
+			fmt.Println("Informe o indice a ser alterado: ")
+			fmt.Scanln(&indice)
+			fmt.Println("Informe o novo email: ")
+			fmt.Scanln(&novoEmail)
+			contatos = operacoes.EditaEmail(indice, contatos, novoEmail)
+
+		case "4":
+			operacoes.ExibirContatos(contatos)
+
+		default:
+			return
+		}
+
+		fmt.Println(contatos)
+	}
+
+}
---
+++ ac3/operacoes/operacoes.go
@@ -0,0 +1,54 @@
+package operacoes
+
+
+import (
+	"fmt"
+	"ac3/contato"
+)
+
+func AdicionaContato(novoContato contato.Contato, a [5]contato.Contato) [5]contato.Contato {
+	for ind, c := range a {
+		if (c == contato.Contato{}) {
+			a[ind] = novoContato
+			break
+		}
+	}
+
+	return a
+}
+
+func ExcluiContato(a [5]contato.Contato) [5]contato.Contato {
+	if (a[0] == contato.Contato{}) {
+		fmt.Println("Lista de contatos vazia!")
+		return a
+	}
+
+	for ind, c := range a {
+		if (c == contato.Contato{}) {
+			a[ind-1] = contato.Contato{}
+		}
+	}
+
+	return a
+}
+
+
+
+//Crie uma nova função editaEmail, que recebe o índice do elemento no array e altera o e-mail do elemento indicado.
+func EditaEmail(indiceContato int, array [5]contato.Contato, novoEmail string) [5]contato.Contato {
+	for ind, c := range array { 
+		if ind == indiceContato {
+			
+			array[ind].Email = novoEmail
+			fmt.Println(c)
+		}
+	}
+
+	return array
+}
+
+func ExibirContatos(array [5]contato.Contato){
+	for ind, c := range array {
+		fmt.Println(ind, c, "\n")
+	}
+}
